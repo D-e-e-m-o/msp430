9a2974512aa1e668976899690b4bc795c02183d1
D-e-e-m-o <wsd009811@gmail.com>
2016-04-14 23:53:50 +0800

    change delay

---
+++ delay.c
@@ -0,0 +1,8 @@
+#include "delay.h"
+
+void delay_ms(uint t)
+{
+  uint i,j;
+  for( i = 0 ; i < t ; i++)
+  for( j = 0 ; j < 1000 ; j++) ;
+}
--- delay.h
+++ delay.h
@@ -1,6 +1,8 @@
-void delay_ms(uint t)
-{
-  uint i,j;
-  for( i = 0 ; i < t ; i++)
-  for( j = 0 ; j < 1000 ; j++) ; 
-}
+#ifndef MSP430_DELAY_H_
+#define MSP430_DELAY_H_
+
+#define uint  unsigned int
+#define uchar unsigned char
+void delay_ms(uint t);
+
+#endif
